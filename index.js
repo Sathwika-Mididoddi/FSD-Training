3591040110328d6de5d6cb92e335cb0e70f9e2ea
Sathwika-Mididoddi <sathwika2665@gmail.com>
2026-06-09 09:29:34 +0530

    added functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+let tbody = document.getElementById("tableBody");
+let students = JSON.parse(localStorage.getItem("students")) || [];
+for (let student of students) {
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `<td>${student.name}</td>
+                          <td>${student.rollno}</td>  
+                          <td>${student.branch}</td>    
+                          <td>${student.cgpa}</td>   
+                          <td><p onclick="editStudent('${student.rollno}')">Edit</p>
+                           <p onclick="deleteStudent('${student.rollno}')">Delete</p></td>         
+                            `;
+    tbody.appendChild(newRow);
+}
+function deleteStudent(rollno) {
+    let students = JSON.parse(localStorage.getItem("students"));
+    students = students.filter(s => s.rollno !== rollno);
+    localStorage.setItem("students", JSON.stringify(students));
+    location.reload();
+}
+function editStudent(rn) {
+    localStorage.setItem("editRollNo", rn);
+    window.location.href="editstudent.html";
+}
