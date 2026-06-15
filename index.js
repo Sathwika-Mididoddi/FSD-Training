f67ce79a7075946ae57aa795faf0242b23adf9fb
Sathwika-Mididoddi <sathwika2665@gmail.com>
2026-06-15 09:37:50 +0530

    added db.json to store data

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 let tbody = document.getElementById("tableBody");
-let students = JSON.parse(localStorage.getItem("students")) || [];
-for (let student of students) {
-    let newRow = document.createElement("tr");
-    newRow.innerHTML = `<td>${student.name}</td>
+//let students = JSON.parse(localStorage.getItem("students")) || [];
+async function getStudents() {
+    let response = await fetch("http://localhost:3000/students");
+    let students = await response.json();
+    for (let student of students) {
+        let newRow = document.createElement("tr");
+        newRow.innerHTML = `<td>${student.name}</td>
                           <td>${student.rollno}</td>  
                           <td>${student.branch}</td>    
                           <td>${student.cgpa}</td>   
-                          <td><p onclick="editStudent('${student.rollno}')">Edit</p>
-                           <p onclick="deleteStudent('${student.rollno}')">Delete</p></td>         
+                          <td><p onclick="editStudent('${student.rollno}')" rollno="${student.rollno}">Edit</p>
+                           <p onclick="deleteStudent('${student.rollno}')" rollno="${student.rollno}">Delete</p></td>         
                             `;
-    tbody.appendChild(newRow);
+        tbody.appendChild(newRow);
+    }
 }
-function deleteStudent(rollno) {
-    let students = JSON.parse(localStorage.getItem("students"));
+getStudents()
+
+async function deleteStudent(rollno) {
+    //let students = JSON.parse(localStorage.getItem("students"));
     //students = students.filter(s => s.rollno !== rollno);
-    let idx=-1;
-    for(i in students)
-    {
-        if(students[i].rollno===rollno)
-        {
-            idx=i;
+    let response = await fetch("http://localhost:3000/students");
+    let students = await response.json();
+    let idx = -1;
+    for (i in students) {
+        console.log(1);
+        if (students[i].rollno === rollno) {
+            idx = students[i].id;
             break;
         }
     }
-    students.splice(idx,1);
-    localStorage.setItem("students", JSON.stringify(students));
+    //students.splice(idx, 1);
+    //localStorage.setItem("students", JSON.stringify(students));
+    let res=await fetch(`http://localhost:3000/students/${idx}`,{
+        method:"DELETE"
+    })
+    console.log(await res.json())
     location.reload();
 }
 function editStudent(rn) {
-    localStorage.setItem("editRollNo", rn);
-    window.location.href="editstudent.html";
+    //localStorage.setItem("editRollNo", rn);
+    const url="editstudent.html";
+    const params=new URLSearchParams({
+        "rollno":rn
+    })
+
+    window.location.href = `editstudent.html?${params.toString()}`;
 }
